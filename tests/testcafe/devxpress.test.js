64a93def3ae47159a80c9c1858b0d945bbd9f1e6
Félix P <felix@saucelabs.com>
2021-03-26 00:12:59 -0700

    Move to copy by default

--- tests/testcafe/devxpress.test.js
+++ tests/testcafe/devxpress.test.js
@@ -1,13 +1,11 @@
 import { Selector } from 'testcafe';
-
 fixture `Getting Started`
-  .page `http://devexpress.github.io/testcafe/example`;
+	.page `http://devexpress.github.io/testcafe/example`
 
 const testName = 'testcafe test'
 test(testName, async t => {
-  await t
-    .typeText('#developer-name', 'devx')
-    .click('#submit-button')
-    // Use the assertion to check if the actual header text is equal to the expected one
-    .expect(Selector('#article-header').innerText).eql('Thank you, devx!');
+	await t
+		.typeText('#developer-name', 'devx')
+		.click('#submit-button')
+		.expect(Selector('#article-header').innerText).eql('Thank you, devx!');
 });
